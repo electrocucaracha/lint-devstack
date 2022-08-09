77e9c49ac11651a885f9aae3ad6a5772f6ae9d1e
Victor Morales <chipahuac@hotmail.com>
2022-08-09 15:25:35 -0700

    Centralize the git repo host value

--- setup.sh
+++ setup.sh
@@ -13,6 +13,7 @@ set -o nounset
 set -o pipefail
 
 PASSWORD='password'
+GIT_REPO_HOST="https://opendev.org/openstack"
 
 # enable_kernel_attr() - Set a kernel attribute to true
 function enable_kernel_attr {
@@ -46,7 +47,7 @@ esac
 curl -fsSL http://bit.ly/install_pkg | PKG_COMMANDS_LIST="sudo,git" bash
 
 if [ ! -d /opt/stack/devstack ]; then
-    sudo -E git clone --depth 1 -b "${DEVSTACK_RELEASE:-stable/yoga}" https://github.com/openstack/devstack /opt/stack/devstack
+    sudo -E git clone --depth 1 -b "${DEVSTACK_RELEASE:-stable/yoga}" "$GIT_REPO_HOST/devstack" /opt/stack/devstack
     sudo chown -R "$USER" /opt/stack/
 fi
 
@@ -85,42 +86,42 @@ EOL
     for arg in "$@"; do
         case $arg in
             "barbican" )
-                echo "enable_plugin barbican https://git.openstack.org/openstack/barbican.git" >> local.conf ;;
+                echo "enable_plugin barbican $GIT_REPO_HOST/barbican.git" >> local.conf ;;
             "ceilometer" )
                 echo "ENABLED_SERVICES+=,ceilometer-api">> local.conf
-                echo "enable_plugin ceilometer https://git.openstack.org/openstack/ceilometer.git" >> local.conf ;;
+                echo "enable_plugin ceilometer $GIT_REPO_HOST/ceilometer.git" >> local.conf ;;
             "cloudkitty" ) # cloudKitty requires ceilometer and horizon
-                echo "enable_plugin cloudkitty https://github.com/openstack/cloudkitty master">> local.conf
+                echo "enable_plugin cloudkitty $GIT_REPO_HOST/cloudkitty master">> local.conf
                 echo "enable_service ck-api ck-proc">> local.conf ;;
             "designate" )
                 echo "ENABLED_SERVICES+=,designate,designate-central,designate-api,designate-pool-manager,designate-zone-manager,designate-mdns">> local.conf
-                echo "enable_plugin designate https://git.openstack.org/openstack/designate">> local.conf ;;
+                echo "enable_plugin designate $GIT_REPO_HOST/designate">> local.conf ;;
             "docker" )
                 echo "VIRT_DRIVER=docker" >> local.conf ;;
             "heat" )
-                echo "enable_plugin heat https://git.openstack.org/openstack/heat.git" >> local.conf ;;
+                echo "enable_plugin heat $GIT_REPO_HOST/heat.git" >> local.conf ;;
             "horizon" )
                 echo "ENABLED_SERVICES+=horizon">> local.conf ;;
             "magnum" ) # magnum requires heat
-                echo "enable_plugin magnum-ui https://git.openstack.org/openstack/magnum-ui">> local.conf
-                echo "enable_plugin magnum https://git.openstack.org/openstack/magnum">> local.conf ;;
+                echo "enable_plugin magnum-ui $GIT_REPO_HOST/magnum-ui">> local.conf
+                echo "enable_plugin magnum $GIT_REPO_HOST/magnum">> local.conf ;;
             "marconi" )
                 echo "ENABLED_SERVICES+=,marconi-server">> local.conf ;;
             "neutron-fwaas" )
                 echo "ENABLED_SERVICES+=,q-fwaas">> local.conf
-                echo "enable_plugin neutron-fwaas https://git.openstack.org/openstack/neutron-fwaas">> local.conf ;;
+                echo "enable_plugin neutron-fwaas $GIT_REPO_HOST/neutron-fwaas">> local.conf ;;
             "neutron-lbaas" )
                 echo "ENABLED_SERVICES+=,q-lbaasv2">> local.conf
-                echo "enable_plugin neutron-lbaas-dashboard https://git.openstack.org/openstack/neutron-lbaas-dashboard">> local.conf
-                echo "enable_plugin neutron-lbaas https://git.openstack.org/openstack/neutron-lbaas">> local.conf ;;
+                echo "enable_plugin neutron-lbaas-dashboard $GIT_REPO_HOST/neutron-lbaas-dashboard">> local.conf
+                echo "enable_plugin neutron-lbaas $GIT_REPO_HOST/neutron-lbaas">> local.conf ;;
             "neutron-metering" )
                 echo "ENABLED_SERVICES+=,q-metering">> local.conf ;;
             "neutron-vpnaas" )
                 echo "ENABLED_SERVICES+=,q-vpnaas">> local.conf
-                echo "enable_plugin neutron-vpnaas https://git.openstack.org/openstack/neutron-vpnaas">> local.conf ;;
+                echo "enable_plugin neutron-vpnaas $GIT_REPO_HOST/neutron-vpnaas">> local.conf ;;
             "octavia" )
                 echo "ENABLED_SERVICES+=,octavia,o-cw,o-hk,o-hm,o-api">> local.conf
-                echo "enable_plugin octavia https://git.openstack.org/openstack/octavia">> local.conf ;;
+                echo "enable_plugin octavia $GIT_REPO_HOST/octavia">> local.conf ;;
             "osprofiler" )
                 echo "CEILOMETER_NOTIFICATION_TOPICS=notifications,profiler" >> local.conf ;;
             "python-neutronclient" )
@@ -128,13 +129,13 @@ EOL
             "python-openstackclient" )
                 echo "LIBS_FROM_GIT+=python-openstackclient" >> local.conf ;;
             "rally" )
-                git clone --depth 1 https://github.com/stackforge/rally /tmp/rally
+                git clone --depth 1 https://opendev.org/stackforge/rally /tmp/rally
                 cp /tmp/rally/contrib/devstack/lib/rally lib/
                 cp /tmp/rally/contrib/devstack/extras.d/70-rally.sh extras.d/
                 echo "ENABLED_SERVICES+=,rally" >> local.conf ;;
             "sahara" ) # sahara requires swift
-                echo "enable_plugin sahara-dashboard git://git.openstack.org/openstack/sahara-dashboard">> local.conf
-                echo "enable_plugin sahara git://git.openstack.org/openstack/sahara">> local.conf ;;
+                echo "enable_plugin sahara-dashboard $GIT_REPO_HOST/sahara-dashboard">> local.conf
+                echo "enable_plugin sahara $GIT_REPO_HOST/sahara">> local.conf ;;
             "swift" )
                 echo "SWIFT_HASH=swift">> local.conf
                 echo "ENABLED_SERVICES+=,s-proxy,s-object,s-container,s-account">> local.conf ;;
